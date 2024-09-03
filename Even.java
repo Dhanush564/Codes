d8de797f29dacf6cbc91b8e3f816e7b71d460606
Dhanush564 <167515494+Dhanush564@users.noreply.github.com>
2024-09-03 09:44:38 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+package mern;
+
+import java.util.Scanner;
+
+public class Even {
+     public static void main(String[] args) {
+    	 Scanner sc=new Scanner(System.in);
+    	 int n=sc.nextInt();
+    	 int i;
+    	 for(i=1;i<n;i++) {
+    		 if(i%2==0) {
+    			 System.out.println(i);
+    		 }
+    	 }
+     }
+}
